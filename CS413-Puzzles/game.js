356a0d26d4b9cb7c73b51b4c3415d8129541c4f2
スーツ芋 <Erekyth@yahoo.com>
2016-05-20 02:09:18 -0700

    Initial game.js

---
+++ CS413-Puzzles/game.js
@@ -0,0 +1,16 @@
+/**********************************************************************************************************
+Keith Saunders Project 2 - Puzzles
+**********************************************************************************************************/
+
+/**********************************************************************************************************
+Aliasing 
+**********************************************************************************************************/
+// Using Aliasing 
+var Container = PIXI.Container,
+	autoDetectRenderer = PIXI.autoDetectRenderer,
+	loader = PIXI.loader,
+	resources = PIXI.loader.resources,
+	TextureCache = PIXI.utils.TextureCache,
+	Texture = PIXI.Texture,
+	Sprite = PIXI.Sprite
+	Text = PIXI.Text;
