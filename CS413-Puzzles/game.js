7ec3ebf4e0f068fb0fa634b1b2b18002c9503d6a
スーツ芋 <Erekyth@yahoo.com>
2016-05-20 02:16:57 -0700

    Loader added

--- CS413-Puzzles/game.js
+++ CS413-Puzzles/game.js
@@ -14,3 +14,24 @@ var Container = PIXI.Container,
 	Texture = PIXI.Texture,
 	Sprite = PIXI.Sprite
 	Text = PIXI.Text;
+
+	
+/**********************************************************************************************************
+Creating the Stage and appending to Gameport
+**********************************************************************************************************/	
+// Creating the PIXI stage and renderer
+var stage = new Container(),
+	renderer = autoDetectRenderer(800, 600, {backgroundColor: 0x000000});
+	
+// Appying to the HTML view
+gameport.appendChild(renderer.view);
+
+/**********************************************************************************************************
+Loader
+**********************************************************************************************************/	
+// Load a JSON file and run the setup function. 
+// 'adds' the JSON sheet
+loader
+	.add("images/sheet.json")
+	//.on("progress", loadProgressHandler)
+	.load(setup);
