2ceca651a0e7880740f4435fe6189907024ac5ba
スーツ芋 <Erekyth@yahoo.com>
2016-05-23 13:58:21 -0700

    Push for TA

--- CS413-Puzzles/game.js
+++ CS413-Puzzles/game.js
@@ -47,7 +47,11 @@ Var Declaration
 ***********************************************************************************************************/
 var state;
 var buttons, start, play, help, credits, back, creditBack;
+var redButton, blueButton, greenButton, yellowButton;
 var says = [];
+var playerSays;
+var playerSaid = [];
+var globalCount = 0;
 
 /**********************************************************************************************************
 Setup Function
@@ -220,6 +224,7 @@ function setup(){
 		redButton.scale.x = 0.8;
 		redButton.scale.y = 0.8;
 		redButton.interactive = true;
+		redButton.on('mousedown', redSays);
 		
 		// Blue Button
 		blueButton.anchor.x = 0.5;
@@ -229,6 +234,7 @@ function setup(){
 		blueButton.scale.x = 0.8;
 		blueButton.scale.y = 0.8;
 		blueButton.interactive = true;
+		blueButton.on('mousedown', blueSays);
 		
 		// Green Button
 		greenButton.anchor.x = 0.5;
@@ -238,6 +244,7 @@ function setup(){
 		greenButton.scale.x = 0.8;
 		greenButton.scale.y = 0.8;
 		greenButton.interactive = true;
+		greenButton.on('mousedown', greenSays);
 		
 		// Yellow Button
 		yellowButton.anchor.x = 0.5;
@@ -247,6 +254,7 @@ function setup(){
 		yellowButton.scale.x = 0.8;
 		yellowButton.scale.y = 0.8;
 		yellowButton.interactive = true;
+		yellowButton.on('mousedown', yellowSays);
 	
 	/*******************************************************************************************************
 	Game Over Scene
@@ -280,7 +288,9 @@ State Functions
 		
 	}
 
-	function help() {
+	function gameFirstPass() {
+		simonSays();
+		state = game;
 		
 	}
 	
@@ -289,7 +299,7 @@ State Functions
 	}
 	
 	function game() {
-		simonSays();
+		player();
 	}
 	
 	function end() {
@@ -306,7 +316,7 @@ Helper Functions
 	
 	function startHandler(e){
 		introScene.visible = false;
-		state = game;
+		state = gameFirstPass;
 		gameScene.visible = true;
 	}
 	
@@ -353,10 +363,49 @@ Helper Functions
 	Red Says
 	*******************************************************************************************************/
 	function redSays(e){
-		introScene.visible = false
-		creditScene.visible = true;
+		globalCount++;
+		playerSays = 1;
+		playerSaid.push(playerSays);
+		console.log(arraysEqual(playerSaid, says));
+		console.log(globalCount == says.length);
 	}
 	
+	/*******************************************************************************************************
+	Blue Says
+	*******************************************************************************************************/
+	function blueSays(e){
+		globalCount++;
+		playerSays = 2;
+		playerSaid.push(playerSays);
+		console.log(arraysEqual(playerSaid, says));
+		console.log(globalCount == says.length);
+		
+	}
+	
+	/*******************************************************************************************************
+	Green Says
+	*******************************************************************************************************/
+	function greenSays(e){
+		globalCount++;
+		playerSays = 3;
+		playerSaid.push(playerSays);
+		console.log(arraysEqual(playerSaid, says));
+		console.log(globalCount == says.length);
+		
+	}
+	
+	
+	/*******************************************************************************************************
+	Yellow Says
+	*******************************************************************************************************/
+	function yellowSays(e){
+		globalCount++;
+		playerSays = 4;
+		playerSaid.push(playerSays);
+		console.log(arraysEqual(playerSaid, says));
+		console.log(globalCount == says.length);
+		
+	}
 	
 	/*******************************************************************************************************
 	Random Integer Function 
@@ -375,36 +424,40 @@ Helper Functions
 	// 4 = Yellow
 
 	function simonSays(){
-
-		//says.push(randomInt(1,4));
 		
+		// Push a number between [1,4]
+		says.push(randomInt(1,4));
+		
+		// Play animations
 		for(var i = 0; i<says.length; i++) {
-			
+			startTimer;
 			// Red 
 			if (says[i] === 1){
 				createjs.Tween.get(redButton.position).to({x: -110, y: -110}, 1000, createjs.Ease.bounceOut)
 				.to({x: -100, y: -100}, 500, createjs.Ease.bounceOut);
+				startTimer;
 			}
 		
-				
 			// Blue
 			else if (says[i] === 2){
 				createjs.Tween.get(blueButton.position).to({x: 110, y: -110}, 1000, createjs.Ease.bounceOut)
 				.to({x: 100, y: -100}, 500, createjs.Ease.bounceOut);
+				startTimer;
 			}
 			
-				
 			// Green
 			else if (says[i] === 3){
 				createjs.Tween.get(greenButton.position).to({x: -110, y: 110}, 1000, createjs.Ease.bounceOut)
 				.to({x: -100, y: 100}, 500, createjs.Ease.bounceOut);
-				break;
+				startTimer;
+				
 			}
 			
 			// Yellow
 			else if (says[i] === 4){
 				createjs.Tween.get(yellowButton.position).to({x: 110, y: 110}, 1000, createjs.Ease.bounceOut)
 				.to({x: 100, y: 100}, 500, createjs.Ease.bounceOut);
+				startTimer;
 			}
 		
 			else { 
@@ -422,7 +475,51 @@ Helper Functions
 		// Repeat simonSays()
 	// Player chooses incorrect button -> End Game.
 	// Player chooses a button why buttons are animating -> Do Nothing
-	
-	
-	
-	
+
+	function player(){
+		
+		redButton.interactive = true;
+		blueButton.interactive = true;
+		greenButton.interactive = true;
+		yellowButton.interactive = true;
+		
+		if(globalCount == says.length){				// Check if Arrays are equal
+			if(arraysEqual(playerSaid, says) == true) {		// If Equal 
+				globalCount = 0;					// reset globalCount
+				redButton.interactive = false;		// set Buttons.interactive to false
+				blueButton.interactive = false;
+				greenButton.interactive = false;
+				yellowButton.interactive = false;
+				simonSays();						// call simonSays
+				
+			}
+			else {
+				alert("lost.");
+			}
+				
+					
+					
+				// Else end game and transit to end game scene
+					// reset globalCount
+					// set Buttons.interactive to false
+					// set says = [];
+			
+		}
+	}
+	/*******************************************************************************************************
+	Timer
+	*******************************************************************************************************/	
+	function startTimer () {
+    timer.start();
+    setTimeout(stopTimer,50000);
+}
+
+	function stopTimer () {
+		timer.stop;
+	}
+	/*******************************************************************************************************
+	Array Equality
+	*******************************************************************************************************/
+	function arraysEqual(a1,a2) {
+		return JSON.stringify(a1)==JSON.stringify(a2);
+	}
